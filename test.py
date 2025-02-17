be06dcf00c6ba0e2821130c7f8aff61e7cf98573
xuguyGIT <xuguy970925@163.com>
2025-02-17 18:37:52 +0800

    fuck1

--- test.py
+++ test.py
@@ -221,7 +221,7 @@ assert isinstance(batch['labels'], torch.LongTensor)
 assert batch['tokens'].shape == (batch_size, 2 * window_size)
 assert batch['labels'].shape == (batch_size,)
 
-
+#%%
 # implement CBoW:
 class CBoWModel(nn.Module):
     def __init__(self, vocab_size, embedding_dim):
@@ -320,7 +320,8 @@ for ep in range(epoch_num):
           total_loss = 0
           start_time = time.time()
 
-
+# =============== finish training ================
+#%%
 embeddings = model.embeddings.weight.data.cpu().numpy()
 # embeddings shape: (7226, 32)
 # does not matter if there is a .cpu() if you are using cpu
@@ -339,3 +340,190 @@ def most_similar(embeddings, index2word, word2index, word):
     return [index2word[index] for index in reversed(top10)]
 
 most_similar(embeddings, index2word, word2index, 'my')
+
+
+# Visualization:
+import bokeh.models as bm, bokeh.plotting as pl
+from bokeh.io import output_notebook
+
+from sklearn.manifold import TSNE
+from sklearn.preprocessing import scale
+
+
+def draw_vectors(x, y, radius=10, alpha=0.25, color='blue',
+                 width=600, height=400, show=True, **kwargs):
+    """ draws an interactive plot for data points with auxilirary info on hover """
+    output_notebook()
+
+    if isinstance(color, str):
+        color = [color] * len(x)
+    data_source = bm.ColumnDataSource({ 'x' : x, 'y' : y, 'color': color, **kwargs })
+
+    fig = pl.figure(active_scroll='wheel_zoom', width=width, height=height)
+    fig.scatter('x', 'y', size=radius, color='color', alpha=alpha, source=data_source)
+
+    fig.add_tools(bm.HoverTool(tooltips=[(key, "@" + key) for key in kwargs.keys()]))
+    if show:
+        pl.show(fig)
+    return fig
+
+
+def get_tsne_projection(word_vectors):
+    tsne = TSNE(n_components=2, verbose=1)
+    return scale(tsne.fit_transform(word_vectors))
+
+
+def visualize_embeddings(embeddings, index2word, word_count):
+    word_vectors = embeddings[1: word_count + 1]
+    words = index2word[1: word_count + 1]
+
+    word_tsne = get_tsne_projection(word_vectors)
+    draw_vectors(word_tsne[:, 0], word_tsne[:, 1], color='blue', token=words)
+
+
+visualize_embeddings(embeddings, index2word, 100)
+
+# %%
+# there is a scipy version conflict here: the scipy version used here is version==1.13, which is over up-to-date, downgrade it to version == 1.12 will work, remeber to reboot kernel
+# import gensim.downloader as api
+'''
+gensim basic data structure source code:
+https://github.com/piskvorky/gensim/blob/develop/gensim/models/keyedvectors.py
+'''
+
+def load_word2vec():
+    """ Load GloVe Twitter Vectors
+        Return:
+            wv_from_bin: Pre-trained embeddings with 25 dimensions for 1.2M vocabulary.
+    """
+    import gensim.downloader as api
+    # glove-twitter-25 (vocab_size, embedding_dim)=(1.2M, 25)
+    wv_from_bin = api.load("glove-twitter-25")
+    vocab = list(wv_from_bin.key_to_index.keys())  # Updated for Gensim 4.x
+    print("Loaded vocab size %i" % len(vocab))
+    return wv_from_bin
+
+
+wv_from_bin = load_word2vec()
+dir(wv_from_bin)
+import pickle
+file_name = 'wv_from_bin.pkl'
+
+# # dump
+# with open(file_name, 'wb') as f:
+#     pickle.dump(wv_from_bin, f)
+
+# re-load
+with open(file_name, 'rb') as f:
+    wv_from_bin = pickle.load(f)
+
+words_tmp = list(wv_from_bin.key_to_index.keys())
+
+# ===== reducing dimensionality =====
+import numpy as np
+
+def get_matrix_of_vectors(wv_from_bin, required_words=['barrels', 'bpd', 'ecuador', 'energy', 'industry', 'kuwait', 'oil', 'output', 'petroleum', 'venezuela']):
+    """ Put the word2vec vectors into a matrix M.
+        Param:
+            wv_from_bin: KeyedVectors object; the 1.2 million word2vec vectors loaded from file
+        Return:
+            M: numpy matrix shape (num words, 300) containing the vectors
+            word2Ind: dictionary mapping each word to its row number in M
+    """
+    import random
+    words = list(wv_from_bin.key_to_index.keys())
+    print("Shuffling words ...")
+    random.shuffle(words)
+    words = words[:10000]
+    print("Putting %i words into word2Ind and matrix M..." % len(words))
+    word2Ind = {}
+    M = []
+    curInd = 0
+    for w in words:
+        try:
+            # wv_from_bin.word_vec(w)将w转换为向量
+            M.append(wv_from_bin.word_vec(w))
+            # 把w映射为indices
+            word2Ind[w] = curInd
+            curInd += 1
+        except KeyError:
+            continue
+    for w in required_words:
+        try:
+            M.append(wv_from_bin.word_vec(w))
+            word2Ind[w] = curInd
+            curInd += 1
+        except KeyError:
+            continue
+    M = np.stack(M)
+    print("Done.")
+    return M, word2Ind
+
+from sklearn.decomposition import TruncatedSVD
+def reduce_to_k_dim(M, k=2):
+    """ Reduce a co-occurrence count matrix of dimensionality (num_corpus_words, num_corpus_words)
+        to a matrix of dimensionality (num_corpus_words, k) using TruncatedSVD.
+
+        Params:
+            M (numpy matrix of shape (num_corpus_words, num_corpus_words)): co-occurrence matrix of word counts
+            k (int): embedding size of each word after dimension reduction
+        Return:
+            M_reduced (numpy matrix of shape (num_corpus_words, k)): matrix of k-dimensional word embeddings.
+    """
+    n_iters = 10  # Number of iterations for SVD
+    print("Running Truncated SVD over %i words..." % (M.shape[0]))
+    
+    # ------------------
+    # Write your implementation here.
+    svd = TruncatedSVD(n_components=k, n_iter=n_iters, random_state=0)  # truncated svd
+    M_reduced = svd.fit_transform(M)  # dimensionality reduction
+
+    # ------------------
+    print("Done.")
+    return M_reduced
+
+import matplotlib.pyplot as plt
+
+def plot_embeddings(M_reduced, word2Ind, words):
+    """ Plot in a scatterplot the embeddings of the words specified in the list "words".
+        Include a label next to each point.
+
+        Params:
+            M_reduced (numpy matrix of shape (number of unique words in the corpus, k)): matrix of k-dimensional word embeddings
+            word2Ind (dict): dictionary that maps word to indices for matrix M
+            words (list of strings): words whose embeddings we want to visualize
+    """
+    plt.figure(figsize=(10, 10))
+    for word in words:
+        if word in word2Ind:
+            idx = word2Ind[word]
+            x, y = M_reduced[idx, 0], M_reduced[idx, 1]
+            plt.scatter(x, y, marker='o', color='blue')
+            plt.text(x + 0.02, y + 0.02, word, fontsize=9)
+        else:
+            print(f"Word '{word}' not found in word2Ind dictionary.")
+
+    plt.title("Word Embeddings Visualization")
+    plt.xlabel("Dimension 1")
+    plt.ylabel("Dimension 2")
+    plt.grid(True)
+    plt.show()
+
+M, word2Ind = get_matrix_of_vectors(wv_from_bin)
+M_reduced = reduce_to_k_dim(M, k=2)
+
+words = ['barrels', 'bpd', 'ecuador', 'energy', 'industry', 'kuwait', 'oil', 'output', 'petroleum', 'venezuela']
+plot_embeddings(M_reduced, word2Ind, words)
+
+'''
+问题：
+1. get_matrix_ 中要求return Return:
+            M: numpy matrix shape (num words, 300) containing the vectors 中的300是为什么？明明wv_from_bin的向量只有25d
+
+2. 为什么要把reqiured_words append到M里？万一M里面包含required_words的embedded vec，有什么影响？
+部分自回答：把required_words加到M里是因为我们想要画出这些words在2维平面的坐标，因此需要把它们加入M，然后一起进行truncatedSVD。猜测：如果M里面已经包含了required_words，因为vocab_size=10000比较大，所以加入10个就算有重复也对我们的的visualization没有多大影响？
+
+3. Find a polysemous word (for example, "leaves" or "scoop") such that the top-10 most similar words (according to cosine similarity) contains related words from both meanings. For example, "leaves" has both "turns" and "ground" in the top 10, and "scoop" has both "buckets" and "pops" 是什么意思？turns和ground有什么联系？随便输入一个词，他的top10里面都有不一样意思的词啊？
+
+4.a
+''' 
